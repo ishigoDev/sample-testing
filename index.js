48317f6348707a4ac1912d122c9952a5c2756ad7
unknown <evincedevapi@gmail.com>
2025-09-12 23:40:14 +0530

    added the inital code PR

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => {
+  res.send("AI Agent is running 🚀");
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
